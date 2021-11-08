697adffd6aef46313ca94398dbaafc13323de583
jennycv <jennycarolinavr@gmail.com>
2021-11-07 22:14:34 -0500

    Backend funcionalidad imagen corregida

--- backend/controllers/ServiciosController.js
+++ backend/controllers/ServiciosController.js
@@ -9,23 +9,25 @@ module.exports = {
     add: async (req, res) => {
 
         const body = req.body;
-        if (req.file != undefined) {
-          body.ImagenPrincipal = "/" + req.file.filename;
-        }else{
-    
+        
         try {
+          if (req.file != undefined) {
+            body.imagenPrincipal = "/" + req.file.filename;
+            console.log(body.imagenPrincipal)
+            // res.status(200).json(body.imagenPrincipal)
+          }
           const registro = await models.Servicio.create(body);
-            res.status(200).json(registro);
-    
+          res.status(200).json(registro);
+          
         } catch (error) {
-    
+          
           return res.status(500).json({
             mensaje: "Ocurrio algo inesperado",
             error,
           });
         }
       }
-    },
+    
     
   
 
