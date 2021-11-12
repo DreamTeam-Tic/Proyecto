5fbce0fe26cca9e7cf02ef6009a248e66a19bde7
jennycv <jennycarolinavr@gmail.com>
2021-11-12 00:38:48 -0500

    Creación de nabvar con logo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,22 @@
+//importar rutas
+import {BrowserRouter as Router,Route} from 'react-router-dom'
 
+import './App.css';
+import Nabvar from './components/Home/Nabvar';
 
 function App() {
   return (
     <div className="App">
 
       <h1>PetsForAlways</h1>
+      <Router>
+      <Nabvar/>
+
+      {/* <Route path='/Formulario' exact component ={Formulario}/> */}
+      <Route path='/' />
+    </Router>
+    
+
       
     </div>
   );
