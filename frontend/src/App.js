c58b787f8f56de838cb327bf060b7794e752614a
MiguelVinasco <miguelvinasco99@gmail.com>
2021-11-14 20:57:17 -0500

    Union de navbar y section tips

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,20 @@
-import SectionTips from './components/Home/SectionTips.jsx'
-
 //importar rutas
-import {BrowserRouter as Router,Route} from 'react-router-dom'
-import './App.css';
-import Footer from './components/Home/Footer';
-import Nabvar from './components/Home/Nabvar';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./App.css";
+import Footer from "./components/Home/Footer";
+import Nabvar from "./components/Home/Nabvar";
+import SectionTips from "./components/Home/SectionTips.jsx";
+
 
 function App() {
-    return (
-      <div className="App">
-
+  return (
+    <div className="App">
       <Router>
-            <Nabvar/>
-              <Route path='/' />
-            </Router>
-            <Footer/>
-      <SectionTips/>
+        <Nabvar />
+        <Route path="/" />
+        <Route path="/" exact component={SectionTips}/>
+        </Router>
+      <Footer />
     </div>
   );
 }
