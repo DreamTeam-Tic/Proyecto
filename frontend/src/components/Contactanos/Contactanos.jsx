8d5ce04fcd228a53123e129c914b9e05a536ebb6
brayan472003@gmail.com <brayan472003@gmail.com>
2021-11-18 13:49:54 -0500

    Cambiando (contactanos)

--- frontend/src/components/Contactanos/Contactanos.jsx
+++ frontend/src/components/Contactanos/Contactanos.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 function Contactanos() {
     return (
-        <div>
-            
+        <div className=".container-md">
+            <div className=".col-md-3">
+                holaa
+            </div>
+            <div className=".col-md-6">
+                holaa
+            </div>
+            <div className=".col-md-3">
+                holaa
+            </div>
         </div>
     )
 }
