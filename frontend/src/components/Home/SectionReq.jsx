75c953007bd6abbdba67b9bd1733695a5505efda
santylink123 <santiagoosorio310@gmail.com>
2021-11-17 20:55:45 -0500

    Actualizando targetas. Añadiendo Boostrap en secciones info y requerimentos

--- frontend/src/components/Home/SectionReq.jsx
+++ frontend/src/components/Home/SectionReq.jsx
@@ -1,35 +1,41 @@
-import React from 'react'
-import olax2 from '../../assets/ola.jpg'
-import '../../styles/sectionreq.css'
+import React from "react";
+import olax2 from "../../assets/img2.jpg";
+import "../../styles/sectionreq.css";
 
 function SectionReq() {
-    return (
-        //aqui debe estar tu componente
-        <div className= "containerx">
-            {/* <h1 id= "tituloextra">Por si se necesita</h1> */}
-
-                <div className="containerimagen">
-                        <img src={olax2} alt="" className="imgsection"/>
-                </div>
-
-            <div className="ContainerTexto">
-                <ul className="texto text-center">
-                    <span id= "subtitulo">Requesitos de adopción:</span><br/><br/>
+  return (
+    //aqui debe estar tu componente
+    <div className="container mt-4">
+      <div className="card mb-3">
+        <div className="row g-0">
+          <div className="col-md-4" id="imgContainer">
+            <img
+              src={olax2}
+              id="imgsection"
+              className="img-fluid rounded-start"
+              alt="..."
+            />
+          </div>
+          <div className="col-md-8">
+            <div className="card-body align-center">
+              <h5 className="card-title fs-3">Requesitos de adopción</h5>
+              <p className="card-text fs-4 text-left">
+                <div className="text-start ml-5" id="text">
+                  <ol>
                     <li>Deseos de tener una mascota.</li>
                     <li>El adoptante debe ser mayor de edad.</li>
                     <li>Mostrar el entorno en el que estara.</li>
                     <li>Capacidad economica para cubrir los gastos.</li>
                     <li>Tiempo para dedicarle a su mascota.</li>
-                    <li>Llenar formulario de adopción.</li>
-                    <li>Compromiso a esterilización.</li>
-                    
-                </ul>
+                    <li>Llenar formulario de adopción. Compromiso a esterilización</li>
+                  </ol>
+                </div>
+              </p>
             </div>
-                    
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
-
-export default SectionReq
-
-
+export default SectionReq;
