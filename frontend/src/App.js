aef639665a821b239c28ade90bf8cd99324de836
santylink123 <santiagoosorio310@gmail.com>
2021-11-23 02:13:05 -0500

    Ajustes en margenes de pagina Nosotros

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,19 +3,15 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/Home/Footer";
 import Nabvar from "./components/Home/Nabvar";
-import Home from "./components/Home/Home"
-import Login from "./components/Login/Login.jsx"
+import Home from "./components/Home/Home";
+import Login from "./components/Login/Login.jsx";
 
-import Donaciones from "./components/Donaciones/Donaciones"
-<<<<<<< HEAD
-import Nosotros from "./components/Nosotros/Nosotros";
-=======
-import Contactanos from "./components/Contactanos/Contactanos.jsx"
+import Donaciones from "./components/Donaciones/Donaciones";
+import Contactanos from "./components/Contactanos/Contactanos.jsx";
 
-import Servicios from "./components/Servicios/Servicios.jsx"
-import Formulario from "./components/Servicios/Formulario.jsx"
-import Nosotros from './components/Nosotros/Nosotros.jsx'
->>>>>>> 1ee6ae178fb616bb2de9a628e0371677b30b570e
+import Servicios from "./components/Servicios/Servicios";
+import Formulario from "./components/Servicios/Formulario.jsx";
+import Nosotros from './components/Nosotros/Nosotros.jsx';
 
 
 function App() {
@@ -25,13 +21,8 @@ function App() {
         <Nabvar />
         
         <Route path="/" exact component={Home}/>
-<<<<<<< HEAD
         <Route path="/nosotros" exact component={Nosotros}/>
-        
-        <Route path="/login" exact component={Login}/>
-=======
         <Route path="/login" exact component={Login}/> 
->>>>>>> 1ee6ae178fb616bb2de9a628e0371677b30b570e
         <Route path="/donaciones" exact component={Donaciones}/>
         <Route path="/contactanos" exact component={Contactanos}/>
         <Route path="/servicios" exact component={Servicios}/>
--- frontend/src/components/Home/SectionInfo.jsx
+++ frontend/src/components/Home/SectionInfo.jsx
@@ -9,7 +9,7 @@ function SectionInfo() {
         <div className="card mb-3">
   <div className="row g-0">
     <div className="col-md-4">
-      <img src={imgInfo} id="imagen" className="img-fluid rounded-start" alt="..."/>
+      <img src={imgInfo} id="imagenInfo" className="img-fluid rounded-start" alt="..."/>
     </div>
     <div className="col-md-8">
       <div className="card-body">
